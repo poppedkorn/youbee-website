1b14b1a0d07d73243947cc0dee82bd0201be9d0e
poppedkorn <m.mahuika@hotmail.com>
2023-09-08 22:33:13 +1200

    initial commit

---
+++ AssessmentJavaScript.cs102.js
@@ -0,0 +1,29 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "20%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  function openLogin() {
+    document.getElementById("loginPage").style.width = "18%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeLogin() {
+    document.getElementById("loginPage").style.width = "0%";
+  }
+
+  function unfade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op < 0.1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 100);
+}
